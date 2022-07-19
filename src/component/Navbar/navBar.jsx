74b738cb754eafd9ec1b36f2e1f8d3299a0eeb4b
Asassxer <81103324+Asassxer@users.noreply.github.com>
2022-07-19 18:13:30 -0500

    Add files via upload

---
+++ src/component/Navbar/navBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const navBar = () => {
+  return (
+   
+    <div> 
+        <button>Menu</button>
+        <button>Productos</button>
+        <button>Nosotros</button>
+        <button>Contacto</button>
+        
+    </div>
+  )
+}
+
+export default navBar
